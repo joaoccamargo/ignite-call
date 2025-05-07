95197e755b4f2454fd270556c0eb72f15d91ce2d
João Camargo <jclaudio00_12@hotmail.com>
2025-05-07 07:26:27 -0300

    configured google auth with nextauth google calendar

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,8 +2,11 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 import { ArrowRight } from '@phosphor-icons/react'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function ConnectCalendar() {
+  const session = useSession()
+
   return (
     <Container>
       <Header>
@@ -15,7 +18,11 @@ export default function ConnectCalendar() {
         <ConnectBox>
           <ConnectItem>
             <Text>Google Calendar</Text>
-            <Button variant="secondary">
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() => signIn('google')}
+            >
               Conectar
               <ArrowRight />
             </Button>
